a16f7210c7cca8c77d3c51b2e90af5b1af29931a
RylerVaughn <Ryler.heyman@icloud.com>
2025-07-22 10:32:42 -0700

    In the process of adding a side panel to the map where you can view all clients and pull up partial modals with specific information about each individual.

--- SolarPanel_Detection_App/core/urls.py
+++ SolarPanel_Detection_App/core/urls.py
@@ -8,5 +8,7 @@ router.register(r"clients", ClientViewSets)
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/", include(router.urls))
+    path("api/", include(router.urls)),
+    path("clients/<int:client_id>/detail/", views.client_detail_modal, name="client_detail_modal"),
+    path("clients/<int:client_id>/addjob/", views.add_job, name="add_job")
 ]
--- SolarPanel_Detection_App/core/views.py
+++ SolarPanel_Detection_App/core/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
-from .models import Client
+from django.shortcuts import render, get_object_or_404, redirect   
+from .models import Client, Job
 from .serializers import ClientSerializer
 from rest_framework import viewsets
 
+
 # Create your views here.
 def index(request):
-    return render(request, "core/index.html")
+    return render(request, "core/index.html", context={"clients": Client.objects.all()})
 
 
 class ClientViewSets(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+def client_detail_modal(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    return render(request, "core/partials/client_detail_modal.html", {"client": client})
+
+
+def add_job(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    payment = request.POST.get("payment")
+    date = request.POST.get("date")
+
+    Job.objects.create(client=client, payment=payment, date=date)
+
+    return redirect("client_detail_modal", client_id=client.id)
