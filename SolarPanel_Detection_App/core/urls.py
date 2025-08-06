854cfc4c7072001d5c08043b2aaf0a25e82d539e
RylerVaughn <Ryler.heyman@icloud.com>
2025-08-06 15:47:21 -0700

    The jobs are now able to be deleted with minimal issues. It looks great the edit menu is basically finished.

--- SolarPanel_Detection_App/core/urls.py
+++ SolarPanel_Detection_App/core/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from .views import ClientViewSets
+from .views import ClientViewSets, JobViewSets
 
-router = routers.DefaultRouter()
-router.register(r"clients", ClientViewSets)
+client_router = routers.DefaultRouter()
+client_router.register(r"clients", ClientViewSets)
+
+job_router = routers.DefaultRouter()
+job_router.register(r"jobs", JobViewSets)
+
+
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/", include(router.urls)),
+    path("api/", include(client_router.urls)),
+    path("api/", include(job_router.urls)),
     path("clients/<int:client_id>/detail/", views.client_detail_modal, name="client_detail_modal"),
     path("clients/<int:client_id>/addjob/", views.add_job, name="add_job"),
     path("clients/addclient/", views.add_client, name="add_client")
